e12f48c26446aa301a17ff10778ba9f196e1c7db
bada13 <bada13@bk.ru>
2021-02-27 08:27:35 +0300

    Initial commit

---
+++ src/main/java/Task1_3.java
@@ -0,0 +1,12 @@
+/** Задания 1-3 по курсу Java2.0
+ *
+ * @author Бахирев Денис
+ * */
+import java.util.Scanner;
+
+public class Task1_3 {
+    public static void main(String[] args) {
+
+    }
+
+}
